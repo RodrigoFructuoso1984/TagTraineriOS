ea7acde617d48994fbe613d5eb477a9af2469e76
Rodrigo Fructuoso <fructuoso.digao@gmail.com>
2023-07-07 14:58:45 -0300

    Instalacao SDk Firebase e GTM

--- Tag Trainer/AppDelegate.swift
+++ Tag Trainer/AppDelegate.swift
@@ -3,17 +3,20 @@
 //  Tag Trainer
 //
 //  Created by Ruth Ellen on 05/02/21.
+//  Edit by Rodrigo Fructuoso on 07/07/23.
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.       
+        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
